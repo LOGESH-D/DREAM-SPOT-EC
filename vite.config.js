978ba5299d75056075192626b652a5a9753e8d8d
LOGESH-D <kit26.csbs30@gmail.com>
2025-07-25 15:38:00 +0530

    second

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss(),],
+  // server: {
+  //   port: 5173,
+  //   host: '0.0.0.0',
+  //   strictPort: true,
+  //   open: true,
+  // },
 })
